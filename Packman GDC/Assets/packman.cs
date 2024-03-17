3aab713f2ae35af15f72fb98364b2fc45cbec86b
HaileyG123 <n11263997@qut.edu.au>
2024-03-17 17:40:30 +1000

    finish set up

--- Packman GDC/Assets/packman.cs
+++ Packman GDC/Assets/packman.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class packman : MonoBehaviour
 {
     float speed = 30;
+    private Vector3 position;
+    
+    //boundary condition
+    float xBoundary = 54f;
+
+    float zBoundary = 50f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,21 +22,54 @@ public class packman : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        position = transform.position;
+        Move();
+        Boundary();
+        transform.position = position;
+    }
+
+    //handles player movement
+    private void Move()
+    {
         if(Input.GetKey(KeyCode.W))
         {
-            this.transform.position += Vector3.forward * speed * Time.deltaTime;
+            position += Vector3.forward * speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            this.transform.position += Vector3.left * speed * Time.deltaTime;
+            position += Vector3.left * speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            this.transform.position += Vector3.right * speed * Time.deltaTime;
+            position += Vector3.right * speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            this.transform.position += Vector3.back * speed * Time.deltaTime;
+            position += Vector3.back * speed * Time.deltaTime;
+        }
+    }
+    
+    //checks if pacman goes passed the game boundary and adjusts accordingly
+    private void Boundary()
+    {
+        //X boundary check
+        if (position.x > xBoundary)
+        {
+            position.x = xBoundary;
+        }
+        else if (this.transform.position.x < -xBoundary)
+        {
+            position.x = -xBoundary;
+        }
+        
+        //z boundary check
+        if (position.z > zBoundary)
+        {
+            position.z = zBoundary;
+        }
+        else if (position.z < -zBoundary)
+        {
+            position.z = -zBoundary;
         }
     }
 
